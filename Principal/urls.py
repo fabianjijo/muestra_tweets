56e9b44b22760f91b76f97d3a5bd43582476d776
fabianji <fabian.jimenez@sansano.usm.cl>
2020-03-10 09:48:54 -0300

    reset

--- Principal/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from Principal.views import PagPrincipal
-from django.urls import path
-
-
-urlpatterns = [
-    url(r"^$", PagPrincipal.as_view(), name='principal'),
-]
